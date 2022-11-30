0150ac41e8fc7dfe9543b04de1352f08c40f5d5e
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-12-01 02:29:58 +0530

    nested if-else

--- Introduction to python.py
+++ Introduction to python.py
@@ -25,3 +25,16 @@ if x%2 == 0:
 else:
     print('Odd')
 print('Done with conditional')
+
+#--------------------------------
+
+#nested if-else
+print('\n#Nested if else')
+print('x =', x)
+if x%2 == 0:
+    if x%3 == 0:
+        print('Divisible by 2 and 3')
+    else:
+        print('Divisible by 2 and not by 3')
+elif x%3 == 0:
+    print('Divisible by 3 and not by 2')
