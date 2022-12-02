f5cb626e4b48a192890afe250f5b8fc93492b65f
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-12-02 22:01:04 +0530

    finding cube root using for loop

--- Simple Numerical Programs.py
+++ Simple Numerical Programs.py
@@ -39,3 +39,15 @@ for j in range(x):
         print(i)
         x=2 #this does not change the x for the 'j' loop, but wherenver the 'i' loop started, it revaluates everytime
         
+#---------------------------------------------
+print("\nFinding cube root of a perfect cube")
+x = int(input("Enter an integer: "))
+for ans in range(0, abs(x)+1):
+    if ans**3 >= abs(x):
+        break
+if ans**3 != abs(x):
+    print(x, 'is not a perfect cube')
+else:
+    if x < 0:
+        ans = -ans
+    print('Cube root of', x, 'is', ans)
